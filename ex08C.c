a19f83cf05c5a44c8f8f4a16cb8592fda4ba2d7b
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-02-28 19:54:25 -0300

    ex08C.c

--- ex08C.c
+++ ex08C.c
@@ -11,30 +11,39 @@ void cabecalho(){
 int main(){
 	setlocale(LC_ALL,"");
 	
-	//Declarando variáveis
+	//Declarando variÃ¡veis
 	int primeiroNumero,segundoNumero;
 	int media,soma,produto,menorValor,maiorValor;
 	
-	//
+	//Pedindo valores
 	printf("Digite o primeiro valor: ");
 	scanf("%i",&primeiroNumero);
 	printf("Digite o segundo valor: ");
 	scanf("%i",&segundoNumero);	
 	cabecalho();
 	
-	//
+	//Calculando valores
 	media = (primeiroNumero + segundoNumero)/2;
 	soma = primeiroNumero + segundoNumero;
 	produto = primeiroNumero * segundoNumero;
-	
-	
-	//
-	printf("Média: %i \n",media);
+
+	//Exibindo dados
+	printf("MÃ©dia: %i \n",media);
 	printf("Soma: %i \n",soma);
 	printf("Produto: %i \n",produto);
-	
-	//
-	
+
+	//Testando maior e menor valor e exibindo dados
+	if(primeiroNumero>segundoNumero){
+		maiorValor = primeiroNumero;
+		menorValor = segundoNumero;
+		printf("Maior valor: %i \n",maiorValor);
+		printf("Menor valor: %i \n",menorValor);
+	}else{
+		maiorValor = segundoNumero;
+		menorValor = primeiroNumero;
+		printf("Maior valor: %i \n",maiorValor);
+		printf("Menor valor: %i \n",menorValor);	
+	}
 	
 	return 0;
 }
