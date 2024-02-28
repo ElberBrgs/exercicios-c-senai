43bab7417e618c5d99cb61e46a0c76a073206464
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-02-28 11:11:43 -0300

    Add files via upload

---
+++ ex07C.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cabecalho(){
+	//Limpa o terminal
+    system("cls || clear"); //por algum motivo só funciona no Dev-C++ 5.11
+	printf("\n===SENAI===\n");
+	fflush(stdin);//Limpa cache - setbuf(stdin,0); no Online GDB
+	}	
+
+int main()
+{
+    //Declarando variáveis
+    char nome [200];
+    int idade;
+    float primeiraNota,segundaNota,terceiraNota;
+    float media;
+    
+    //Pedindo dados
+    cabecalho();
+    printf("Digite seu nome: ");
+    scanf("%s",nome);
+    cabecalho();
+	printf("Digite sua idade: ");
+    scanf("%i",&idade);
+    
+    cabecalho();
+    printf("Primeira Nota: ");
+    scanf("%f",&primeiraNota);
+    printf("Segunda Nota: ");
+    scanf("%f",&segundaNota);
+    printf("Terceira Nota: ");
+    scanf("%f",&terceiraNota);
+    cabecalho();
+    
+    //Calculando média
+    media = (primeiraNota+segundaNota+terceiraNota)/3; 
+    
+    //Exibindo dados
+    printf("Nome: %s \n",nome);
+    printf("Idade: %i \n",idade);
+    printf("Sua média é: %.2f \n",media);
+    
+	//Testando média
+    if(media>=7){
+    	printf("Aprovado! \n");
+	}else{
+		printf("Reprovado! \n"); 
+	}    
+
+    return 0;
+}
+
