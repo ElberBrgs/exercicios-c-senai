3268bce56eefb323a302a9b9b569cf6a30fb306c
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-02-28 08:49:37 -0300

    Add files via upload

---
+++ ex02CRefatorado.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cabecalho(){
+	//Limpa o terminal
+    system("cls || clear"); //por algum motivo só funciona no Dev-C++ 5.11
+	printf("\n===SENAI===\n");
+	}	
+
+int main()
+{
+    //Declarando variáveis
+    char nome [200];
+    int idade;
+    float primeiraNota,segundaNota,terceiraNota,quartaNota;
+    float media;
+    
+    //Pedindo dados
+    cabecalho();
+    printf("Digite seu nome: ");
+    scanf("%s",nome);
+    cabecalho();
+	printf("Digite sua idade: ");
+    scanf("%i",&idade);
+    
+    cabecalho();
+    printf("Primeira Nota: ");
+    scanf("%f",&primeiraNota);
+    printf("Segunda Nota: ");
+    scanf("%f",&segundaNota);
+    printf("Terceira Nota: ");
+    scanf("%f",&terceiraNota);
+    printf("Quarta Nota: ");
+    scanf("%f",&quartaNota);
+    cabecalho();
+    
+    //Calculando média
+    media = (primeiraNota+segundaNota+terceiraNota+quartaNota)/4; 
+    
+    //Exibindo dados
+    printf("Nome: %s \n",nome);
+    printf("Idade: %i \n",idade);
+    printf("Nota 1: %.2f \n",primeiraNota);
+    printf("Nota 2: %.2f \n",segundaNota);
+    printf("Nota 3: %.2f \n",terceiraNota);
+    printf("Nota 4: %.2f \n",quartaNota);
+    printf("Sua média é: %.2f \n",media);
+    
+
+    return 0;
+}
+
