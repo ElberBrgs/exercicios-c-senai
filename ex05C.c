d61e6e7d67559032476962a08db8324e8c824947
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-02-28 08:18:38 -0300

    Add files via upload

---
+++ ex05C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	//Declarando variáveis
+	float salario,salarioMinimo,quantidadeSalariosMin;
+	
+	//Pedindo dados
+	printf("Digite seu salário mensal: R$ ");
+	scanf("%f",&salario);
+	
+	//Calculando e exibindo dados
+	salarioMinimo = 1412;
+	quantidadeSalariosMin = salario / salarioMinimo;
+	printf("Salários mínimos ganhos: %.0f \n",quantidadeSalariosMin);
+	
+}
