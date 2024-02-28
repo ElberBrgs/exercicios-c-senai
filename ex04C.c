36d70e2508875a53f3a7078296ea89ca90b4ee81
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-02-27 23:21:36 -0300

    ex04C.c

---
+++ ex04C.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Declarando variáveis
+    int numero,antecessor,sucessor;
+    
+    //Pedindo valores
+    printf("Digite um número: ");
+    scanf("%i",&numero);
+    
+    //Testando valores e exibindo resultados
+    if(numero>0){
+        antecessor = numero - 1;
+        sucessor = numero + 1;
+        printf("Antecessor: %i \n",antecessor);
+        printf("Sucessor: %i \n",sucessor);
+        
+    }else{
+        printf("Número sem antecessor. \n");
+        sucessor = numero + 1;
+        printf("Sucessor: %i \n",sucessor);
+    };
+    
+    
+
+    return 0;
+}
