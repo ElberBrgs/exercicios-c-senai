891ba14d059f0a06c9668b33e423375fe7491db1
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-02-28 08:20:05 -0300

    ex05C.c

--- ex05C.c
+++ ex05C.c
@@ -2,16 +2,17 @@
 
 int main(){
 	
-	//Declarando vari·veis
+	//Declarando vari√°veis
 	float salario,salarioMinimo,quantidadeSalariosMin;
 	
 	//Pedindo dados
-	printf("Digite seu sal·rio mensal: R$ ");
+	printf("Digite seu sal√°rio mensal: R$ ");
 	scanf("%f",&salario);
 	
 	//Calculando e exibindo dados
 	salarioMinimo = 1412;
 	quantidadeSalariosMin = salario / salarioMinimo;
-	printf("Sal·rios mÌnimos ganhos: %.0f \n",quantidadeSalariosMin);
+	printf("Sal√°rios m√≠nimos ganhos: %.0f \n",quantidadeSalariosMin);
 	
+	return 0;
 }
