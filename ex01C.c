3f4d3129fec89cc1522a79f162cb7194893d689a
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-02-28 21:34:16 -0300

    ex001C.c

--- ex01C.c
+++ ex01C.c
@@ -3,16 +3,16 @@
 
 int main(){
 	
-	//Declarando vari·veis
+	//Declarando vari√°veis
 	int primeiroNum,segundoNum;
 	int soma,subtracao,multiplicacao,divisao;
 	
 	//Pedindo valores
-	printf("Digite o primeiro n˙mero: ");
+	printf("Digite o primeiro n√∫mero: ");
 	scanf("%i",&primeiroNum);
-	printf("Digite o segundo n˙mero: ");
+	printf("Digite o segundo n√∫mero: ");
 	scanf("%i",&segundoNum);
-	//Fazendo c·lculos
+	//Fazendo c√°lculos
 	soma = primeiroNum + segundoNum;
 	subtracao = primeiroNum - segundoNum;
 	multiplicacao = primeiroNum * segundoNum;
@@ -20,8 +20,8 @@ int main(){
 	//Exibindo resultados
 	printf("Soma: %i \n",soma);
 	printf("Subtracao: %i \n",subtracao);
-	printf("MultiplicaÁ„o: %i \n",multiplicacao);
-	printf("Divis„o: %i \n",divisao);
+	printf("Multiplica√ß√£o: %i \n",multiplicacao);
+	printf("Divis√£o: %i \n",divisao);
 	
 	return 0;
 	
