8810c0d55637482de5f25d4dd28101f099c38a8d
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-02-29 10:40:54 -0300

    contadorCrescenteC.c

---
+++ contadorCrescenteC.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void cabecalho(){
+	system("cls || clear");
+	printf("\n===SENAI===\n");
+	fflush(stdin);
+}
+
+int main(){
+	setlocale(LC_ALL,""); 
+	
+	
+	int i;
+	
+	for(i=1;i>=100;i--){
+		printf("%d \n",i);
+	}
+	
+	return 0;
+}
