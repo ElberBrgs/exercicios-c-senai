b62fb8a908c79e198e5fa9fe3123e039e2f0b8b2
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-02-29 10:40:10 -0300

    contadorDecrescenteC.c

---
+++ contadorDecrescenteC.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void cabecalho(){
+	system("cls || clear");
+	printf("\n===SENAI===\n");
+	fflush(stdin);
+}
+
+int main(){
+	setlocale(LC_ALL,""); 
+	
+	
+	int j;
+	
+	for(j=100;j>=1;j--){
+		printf("%d \n",j);
+	}
+	
+	return 0;
+}
