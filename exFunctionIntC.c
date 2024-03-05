d6c13dac84e88c28f5f361975337f3841362f59d
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-03-05 10:55:57 -0300

    exFunctionIntC.c

---
+++ exFunctionIntC.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int somar(int n1,int n2){
+	int soma;	
+	soma = n1 + n2;
+	return soma;
+	
+}
+
+
+int main(){
+	setlocale(LC_ALL,""); 
+	
+	int primeiroNum,segundoNum,soma;
+	
+	printf("Digite o primeiro número: ");
+	scanf("%i",&primeiroNum);
+
+	printf("Digite o segundo número: ");
+	scanf("%i",&segundoNum);
+	
+	soma = somar(primeiroNum,segundoNum);	
+	
+	
+	printf("\nSoma: %i \n ",soma);
+	
+	return 0;
+}
