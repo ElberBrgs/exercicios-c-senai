8f21c56664977d18e45b607c61177b9a5c58df69
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-03-05 09:31:05 -0300

    ex015C.c

---
+++ ex015C.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void cabecalho(){
+	system("cls || clear");
+	printf("\n===SENAI===\n");
+	fflush(stdin);
+}
+
+int main(){
+	setlocale(LC_ALL,""); 
+	cabecalho();
+	
+	
+	int numero,i,pares=0,impares=0;
+	
+	for(i=1 ; i<=5; i++){
+		printf("Escreva o %iº valor: ",i);
+		scanf("%i",&numero);
+		
+		if(numero %2 == 0){
+			pares ++;
+		}else{
+			impares ++;
+		}
+	}
+		printf("Pares: %i \n",pares);
+		printf("Ímpares: %i \n",impares);
+	
+			return 0;
+}
