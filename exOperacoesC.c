cdbfa2ee5cd32b747a14b2e31a1d2e277fa72cc8
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-03-07 08:23:52 -0300

    exOperacoesC.c

---
+++ exOperacoesC.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int somar(int n1,int n2){
+	int soma;
+	soma = n1 + n2;
+	return soma;
+	
+}
+int subtrair(int n1,int n2){
+	int subtracao;
+	subtracao = n1 - n2;
+	return subtracao;
+	
+}
+int dividir(int n1,int n2){
+	int divisao;
+	divisao = n1 / n2;
+	return divisao;
+	
+}
+int multiplicar(int n1,int n2){
+	int multiplicacao;
+	multiplicacao = n1 * n2;
+	return multiplicacao;
+	
+}
+
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	int primeiroNum,segundoNum,soma,subtracao,divisao,multiplicacao;
+	
+	printf("Digite o primeiro número: ");
+	scanf("%i",&primeiroNum);
+	printf("Digite o segundo número: ");
+	scanf("%i",&segundoNum);
+	
+	soma = somar(primeiroNum,segundoNum);
+	subtracao = subtrair(primeiroNum,segundoNum);
+	divisao = dividir(primeiroNum,segundoNum);
+	multiplicacao = multiplicar(primeiroNum,segundoNum);
+	
+	printf("\nSoma: %i \n",soma);
+	printf("\nSubtração: %i \n",subtracao);
+	printf("\nDivisão: %i \n",divisao);
+	printf("\nMultiplicação: %i \n",multiplicacao);
+	
+	
+	return 0;
+}
