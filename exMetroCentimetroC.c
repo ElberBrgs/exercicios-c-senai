a04c4e4e8e775f011fe775e79ba3688156b67333
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-03-07 10:29:33 -0300

    exMetroCentimetroC.c

---
+++ exMetroCentimetroC.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+float converterCm(float cMetro,float metro){
+	
+	cMetro = metro * 100;
+	
+	return cMetro;
+}
+
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	float metros,centimetros;
+	
+	printf("Metros: ");
+	scanf("%f",&metros);
+	
+	metros = converterCm(centimetros,metros);
+	
+	printf("Em centímetros: %.2f",metros);
+	
+	return 0;
+}
