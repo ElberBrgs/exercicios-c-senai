2fed7e4b892a067143cb8c4406cf76a92a93c8c4
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-03-07 10:13:06 -0300

    exFunctionCharC.c

---
+++ exFunctionCharC.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+char* verificarParOuImpar(int numero){
+	char resposta [200];
+	
+	if(numero % 2 == 0){
+		strcpy(resposta,"Número Par.");
+	}else{
+		strcpy(resposta,"Número Ímpar.");
+	}
+	
+}
+
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	int numero;
+	char resultado[200];
+	
+	printf("Digite um número: ");
+	scanf("%i",&numero);
+	
+	strcpy(resultado, verificarParOuImpar(numero));
+	printf("Resultado: %s \n",resultado);
+	
+	return 0;
+}
