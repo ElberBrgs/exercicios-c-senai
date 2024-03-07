421f2d2bdeedcfdc495939daabfc24626aa603b8
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-03-07 11:09:59 -0300

    exInflacaoC.c

---
+++ exInflacaoC.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+float inflacionarPreco(float preco){
+	float novoPreco;
+	
+	if(preco < 100){
+		novoPreco = preco * 1.1;
+		
+	}else if(preco >=100){
+		novoPreco = preco * 1.2;	
+	}
+	
+	return novoPreco;
+}
+
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	 float precoProduto,resultado;
+	 
+	 printf("Digite o preço do produto: ");
+	 scanf("%f",&precoProduto);
+	 
+	 resultado = inflacionarPreco(precoProduto);
+	 printf("Resultado: %.2f",resultado);
+		
+	return 0;
+}
