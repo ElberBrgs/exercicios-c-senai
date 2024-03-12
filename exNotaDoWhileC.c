8eda5040259e82bbe63c8db098e45708532ff0e4
Elbo <88691164+ElberBrgs@users.noreply.github.com>
2024-03-12 10:39:07 -0300

    exNotaDoWhileC.c

---
+++ exNotaDoWhileC.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void cabecalho(){
+	system("cls || clear");
+	printf("\n===SENAI===\n");
+	fflush(stdin);
+}
+
+int main(){
+	setlocale(LC_ALL,""); 
+	cabecalho();
+	
+	float nota;
+	
+	
+	do{
+		
+	printf("Digite a sua nota: ");
+	scanf("%f",&nota);
+	
+	cabecalho();
+		
+	}while(nota < 0 || nota > 10);
+	
+	printf("Nota: %.1f \n",nota);
+	printf("FIM.");
+	
+	return 0;
+}
