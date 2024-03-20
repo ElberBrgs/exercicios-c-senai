954d7494063e25ccb420d849a9611d4c0f9e0d47
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-03-20 10:20:17 -0300

    exMediaArrayC.c

---
+++ exMediaArrayC.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	int i;
+	float notas[3],media,soma=0;
+	
+	printf("=== Digite suas notas === \n");
+	for (i = 0; i<3; i++){
+		printf("%dª nota: ",i + 1);
+		scanf("%f",&notas[i]);
+		
+		
+	}
+	soma = notas[0] + notas[1] + notas[2];
+	
+	system("clear || cls");
+	
+	
+	printf("=== Notas === \n");
+	
+	for (i = 0; i <3; i++){
+		printf("%dª nota: %.1f\n", i + 1,notas[i]);
+	}
+	media = (float)soma / i;
+	
+	printf("Média: %.1f",media);
+	
+	return 0;
+	
+}
