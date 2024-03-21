707b0f3042f62693b54cb78b87b0238915e49d63
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-03-21 09:24:18 -0300

    exPessoasArrayC.c

---
+++ exPessoasArrayC.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define TAM 2 // Constante de tamanho.
+
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	char pessoas[TAM][200];
+	int i,idade[TAM];
+	
+	for(i=0;i< TAM ;i++){
+		printf("Digite a %dª pessoa: ",i+1);
+		scanf("%s",&pessoas[i]);
+		printf("Digite a idade: ",i+1);
+		scanf("%d",&idade[i]);
+	}
+	
+	printf("\n===Exibindo resultados===\n");
+	
+	for(i=0; i<TAM;i++){
+		printf("Nome: %s\n",pessoas[i]);
+		printf("Idade: %d\n",idade[i]);
+	}
+	
+	return 0;
+}
