c9518434c1c1a38b7e2a667ca7519bf725079b56
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-04-04 11:27:10 -0300

    exMatrizDisciplinasC.c

---
+++ exMatrizDisciplinasC.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define DISCIPLINA 3 //Constante das disciplinas
+#define NOTAS 2 // Constante das notas
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	char disciplina [DISCIPLINA] [200]; //Vetor para 3 disciplinas.
+	float notas [DISCIPLINA] [NOTAS]; //Matriz com 2 notas para três disciplinas.
+	float soma=0,media[4];
+	int i,j;
+	
+	for(i=0;i<DISCIPLINA;i++){
+		printf("Digite o nome da %dª disciplina: ",i+1);
+		scanf("%s",&disciplina[i]);
+		
+		fflush(stdin);
+		
+		for(j=0;j<NOTAS;j++){
+			printf("Digite a %dª nota:",j+1);
+			scanf("%f",&notas[i][j]);
+			
+			soma += notas[i][j];
+		}
+		
+		media[i] = soma /j;
+		soma = 0; //Zera a variável soma a cada laço.
+				
+		printf("\n"); 
+	}
+
+	system("cls || clear");
+	printf("\n===Exibindo resultados===\n");
+	
+	for(i=0;i<DISCIPLINA;i++){
+		printf("Nome da disciplina: %s \n",disciplina[i]);
+		
+		fflush(stdin);
+		
+		for(j=0;j<NOTAS;j++){
+			printf("%dª nota: %.1f \n",j+1,notas[i][j]);
+		}
+		printf("Média: %.1f \n",media[i]);
+		printf("\n");
+		
+	}
+		
+	return 0;
+}
