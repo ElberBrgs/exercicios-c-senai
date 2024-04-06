f4de816f066fc14968c2a69fc3da6e516c0f83cd
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-04-06 20:58:36 -0300

    ex002C.c

--- ex002C.c
+++ ex002C.c
@@ -1,52 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cabecalho(){
-	//Limpa o terminal
-    system("cls || clear"); //por algum motivo só funciona no Dev-C++ 5.11
-	printf("\n===SENAI===\n");
-	}	
+void cabecalho() {
+    system("cls || clear");
+    printf("\n===SENAI===\n");
+}
 
-int main()
-{
-    //Declarando variáveis
-    char nome [200];
+int main() {
+    // Declarando variáveis
+    char nome[200];
     int idade;
-    float primeiraNota,segundaNota,terceiraNota,quartaNota;
+    float primeiraNota, segundaNota, terceiraNota, quartaNota;
     float media;
     
-    //Pedindo dados
+    // Pedindo dados
     cabecalho();
     printf("Digite seu nome: ");
-    scanf("%s",nome);
+    scanf("%s", nome);
     cabecalho();
-	printf("Digite sua idade: ");
-    scanf("%i",&idade);
+    printf("Digite sua idade: ");
+    scanf("%i", &idade);
     
     cabecalho();
     printf("Primeira Nota: ");
-    scanf("%f",&primeiraNota);
+    scanf("%f", &primeiraNota);
     printf("Segunda Nota: ");
-    scanf("%f",&segundaNota);
+    scanf("%f", &segundaNota);
     printf("Terceira Nota: ");
-    scanf("%f",&terceiraNota);
+    scanf("%f", &terceiraNota);
     printf("Quarta Nota: ");
-    scanf("%f",&quartaNota);
+    scanf("%f", &quartaNota);
     cabecalho();
     
-    //Calculando média
-    media = (primeiraNota+segundaNota+terceiraNota+quartaNota)/4; 
+    // Calculando média
+    media = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4; 
     
-    //Exibindo dados
-    printf("Nome: %s \n",nome);
-    printf("Idade: %i \n",idade);
-    printf("Nota 1: %.2f \n",primeiraNota);
-    printf("Nota 2: %.2f \n",segundaNota);
-    printf("Nota 3: %.2f \n",terceiraNota);
-    printf("Nota 4: %.2f \n",quartaNota);
-    printf("Sua média é: %.2f \n",media);
-    
+    // Exibindo dados
+    printf("Nome: %s \n", nome);
+    printf("Idade: %i \n", idade);
+    printf("Nota 1: %.2f \n", primeiraNota);
+    printf("Nota 2: %.2f \n", segundaNota);
+    printf("Nota 3: %.2f \n", terceiraNota);
+    printf("Nota 4: %.2f \n", quartaNota);
+    printf("Sua média é: %.2f \n", media);
 
     return 0;
 }
-
