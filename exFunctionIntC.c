8f5be3d7cc439ae4a7ab412874be4d421f3e2bf4
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-04-06 21:13:25 -0300

    exFunctionIntC.c

--- exFunctionIntC.c
+++ exFunctionIntC.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <locale.h>
 
-//USAR RETURN COM BASE NO TIPO DE NUMERO CASO TENHA CÁLCULOS MATEMÁTICOS
 int somar(int n1,int n2){
 	int somaFunction;	
 	somaFunction = n1 + n2;
