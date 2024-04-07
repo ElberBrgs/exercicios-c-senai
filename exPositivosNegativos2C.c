7a7c921edfab2d63adf84488b4bc7b1e1d0ce777
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-04-06 21:43:09 -0300

    exPositivosNegativos2C.c

--- exPositivosNegativos2C.c
+++ exPositivosNegativos2C.c
@@ -4,14 +4,11 @@
 
 #define TAM 10 // Constante de tamanho.
 
-
 int main(){
 	setlocale(LC_ALL,"");
 	
 	float numeros[TAM],somaPositivos=0;
 	int i,quantidadeNegativos=0;
-	
-	
 	
 	for(i=0;i<TAM ;i++){
 		
@@ -23,14 +20,12 @@ int main(){
 		}
 		if(numeros[i]<0){
 			quantidadeNegativos++;
-		}
-		
+		}	
 	}
 	
 	printf("\n===Exibindo resultados===\n");
 	printf("Quantidade de números negativos: %i\n",quantidadeNegativos);
 	printf("Soma dos números positivos: %.1f",somaPositivos);
-	
 		
 	return 0;
 }
