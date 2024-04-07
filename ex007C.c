4474665a551aa1329b5f1f154d653559cac31a4f
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-04-06 21:01:54 -0300

    ex007C.c

--- ex007C.c
+++ ex007C.c
@@ -1,53 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cabecalho(){
-	//Limpa o terminal
-    system("cls || clear"); //por algum motivo só funciona no Dev-C++ 5.11
-	printf("\n===SENAI===\n");
-	fflush(stdin);//Limpa cache - setbuf(stdin,0); no Online GDB
-	}	
+void cabecalho() {
+    system("cls || clear");
+    printf("\n===SENAI===\n");
+    fflush(stdin);
+}
 
-int main()
-{
-    //Declarando variáveis
-    char nome [200];
+int main() {
+    // Declarando variáveis
+    char nome[200];
     int idade;
-    float primeiraNota,segundaNota,terceiraNota;
+    float primeiraNota, segundaNota, terceiraNota;
     float media;
     
-    //Pedindo dados
+    // Pedindo dados
     cabecalho();
     printf("Digite seu nome: ");
-    scanf("%s",nome);
+    scanf("%s", nome);
     cabecalho();
-	printf("Digite sua idade: ");
-    scanf("%i",&idade);
+    printf("Digite sua idade: ");
+    scanf("%i", &idade);
     
     cabecalho();
     printf("Primeira Nota: ");
-    scanf("%f",&primeiraNota);
+    scanf("%f", &primeiraNota);
     printf("Segunda Nota: ");
-    scanf("%f",&segundaNota);
+    scanf("%f", &segundaNota);
     printf("Terceira Nota: ");
-    scanf("%f",&terceiraNota);
+    scanf("%f", &terceiraNota);
     cabecalho();
     
-    //Calculando média
-    media = (primeiraNota+segundaNota+terceiraNota)/3; 
+    // Calculando média
+    media = (primeiraNota + segundaNota + terceiraNota) / 3; 
     
-    //Exibindo dados
-    printf("Nome: %s \n",nome);
-    printf("Idade: %i \n",idade);
-    printf("Sua média é: %.2f \n",media);
+    // Exibindo dados
+    printf("Nome: %s \n", nome);
+    printf("Idade: %i \n", idade);
+    printf("Sua média é: %.2f \n", media);
     
-	//Testando média
-    if(media>=7){
-    	printf("Aprovado! \n");
-	}else{
-		printf("Reprovado! \n"); 
-	}    
+    // Testando média
+    if(media >= 7) {
+        printf("Aprovado! \n");
+    } else {
+        printf("Reprovado! \n"); 
+    }    
 
     return 0;
 }
-
