85a477760a9873a8be7399abb2f7aee69e40e971
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-04-06 21:16:34 -0300

    exImparParArrayC.c

--- exImparParArrayC.c
+++ exImparParArrayC.c
@@ -2,18 +2,15 @@
 #include <stdlib.h>
 #include <locale.h>
 
-
 int main(){
 	setlocale(LC_ALL,"");
 	
 	int numeros [6],i,par=0,impar=0;
 	
-	
 	for(i=0;i<6;i++){
 		printf("Digite o %dº número: ",i+1);
 		scanf("%d",&numeros[i]);
 		
-	
 	if(numeros[i] %2 == 0){
 		par ++;	
 	}else {
@@ -28,10 +25,8 @@ int main(){
 		printf("%dº número: %d \n",i+1,numeros[i]);
 	}
 	
-	
 	printf("Números par: %d\n",par);	
 	printf("Números ímpar: %d",impar);
-
-		
+	
 	return 0;
 }
