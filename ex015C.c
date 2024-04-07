37fdf7f107c2ff763020a001f247063c083b6682
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-04-06 21:06:00 -0300

    ex015C.c

--- ex015C.c
+++ ex015C.c
@@ -2,31 +2,30 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void cabecalho(){
-	system("cls || clear");
-	printf("\n===SENAI===\n");
-	fflush(stdin);
+void cabecalho() {
+    system("cls || clear");
+    printf("\n===SENAI===\n");
+    fflush(stdin);
 }
 
-int main(){
-	setlocale(LC_ALL,""); 
-	cabecalho();
+int main() {
+    setlocale(LC_ALL, ""); 
+    cabecalho();
 	
+    int numero, i, pares = 0, impares = 0;
 	
-	int numero,i,pares=0,impares=0;
-	
-	for(i=1 ; i<=5; i++){
-		printf("Escreva o %iº valor: ",i);
-		scanf("%i",&numero);
+    for(i = 1; i <= 5; i++) {
+        printf("Escreva o %iº valor: ", i);
+        scanf("%i", &numero);
 		
-		if(numero %2 == 0){
-			pares ++;
-		}else{
-			impares ++;
-		}
-	}
-		printf("Pares: %i \n",pares);
-		printf("Ímpares: %i \n",impares);
+        if(numero % 2 == 0) {
+            pares++;
+        } else {
+            impares++;
+        }
+    }
+    printf("Pares: %i \n", pares);
+    printf("Ímpares: %i \n", impares);
 	
-			return 0;
+    return 0;
 }
