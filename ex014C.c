ac85a44264913cd662ce9618623ecd440aa8d87b
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-04-06 21:04:53 -0300

    ex014C.c

--- ex014C.c
+++ ex014C.c
@@ -2,32 +2,30 @@
 #include <stdlib.h>
 #include <locale.h>
 
-void cabecalho(){
-	system("cls || clear");
-	printf("\n===SENAI===\n");
-	fflush(stdin);
+void cabecalho() {
+    system("cls || clear");
+    printf("\n===SENAI===\n");
+    fflush(stdin);
 }
 
-int main(){
-	setlocale(LC_ALL,""); 
-	cabecalho();
+int main() {
+    setlocale(LC_ALL, ""); 
+    cabecalho();
 	
-	//Declarando variáveis.
-	int nota, soma, i;
+    // Declarando variáveis.
+    int nota, soma = 0, i;
 	
-	//Solicitando dados para o usuário.
-	for(i=1; i<=5; i++){
-		printf("Digite a %iª nota: ", i);
-		scanf("%i",&nota);
+    // Solicitando dados para o usuário.
+    for(i = 1; i <= 5; i++) {
+        printf("Digite a %iª nota: ", i);
+        scanf("%i", &nota);
 
-    //Somando notas
-		soma = soma + nota;
-	}
+        // Somando notas
+        soma = soma + nota;
+    }
 	
+    // Exibindo resultado.
+    printf("\nSoma: %i \n", soma);
 	
-	//Exibindo resultado.
-	printf("\nSoma: %i \n",soma);
-	
-	
-	return 0;
+    return 0;
 }
