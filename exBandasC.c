b58c2bdefa99ceeda6c9fe4abd965ebd5de4a721
Élber Borges <88691164+ElberBrgs@users.noreply.github.com>
2024-04-10 20:23:22 -0300

    exBandasC.c

--- exBandasC.c
+++ exBandasC.c
@@ -2,35 +2,34 @@
 #include <stdlib.h>
 #include <locale.h>
 
-#define BANDAS 2
-#define INTEGRANTES 2 
+#define BANDAS 3
+#define INTEGRANTES 5 
 
 int main(){
 	setlocale(LC_ALL,"");
 	
-	char bandas[BANDAS][200],integrantes[INTEGRANTES][200];
+	char bandas[BANDAS][200]; // Vetor com a quantidade de bandas.
+	char integrantes[BANDAS][INTEGRANTES][200]; //Matriz com a quantidade de bandas e integrantes.
 	int i,j;
 	
 	for (i=0;i<BANDAS;i++){ 
 		printf("Digite a %dª banda:\n",i+1);
-		scanf("%s",&bandas[i]);
-		fflush(stdin);
-			
+		gets(bandas[i]);
+		
 		for(j=0;j<INTEGRANTES;j++){
-			printf("Digite o %dº integrante:\n",j+1);
-			scanf("%s",&integrantes[i][j]);
+			printf("Digite o %dº integrante da %dº banda:\n",j+1,i+1);
+			gets(integrantes[i][j]);
 		}
 	}
 	
+	system("clear || cls");
 	printf("===Exibindo resultados===\n");
 	
-	for(i=0;i<BANDAS;i++){ //Laço para exibição
+	for(i=0;i<BANDAS;i++){ 
 		printf("%dª banda: %s\n",i+1,bandas[i]);
 		
-		fflush(stdin);
-		
 		for(j=0;j<INTEGRANTES;j++){
-		printf("%dº integrante: %s\n",j+1,integrantes[j]);
+		printf("%dº integrante da %dª banda: %s\n",j+1,i+1,integrantes[i][j]);
 		}
 	}
 		
